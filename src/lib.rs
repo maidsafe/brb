9e4bb350b21ae282b9f925ae0c7daf4928193578
danda <dan-da@users.noreply.github.com>
2020-11-16 17:07:44 -0800

    Add SecureBroadcastNetwork trait

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,8 @@ pub use secure_broadcast_algorithm::SecureBroadcastAlgorithm;
 mod secure_broadcast_impl;
 pub use secure_broadcast_impl::SecureBroadcastImpl;
 
+mod secure_broadcast_network;
+pub use secure_broadcast_network::SecureBroadcastNetwork;
+
 mod packet;
 pub use packet::{Packet, Payload, Msg, BFTOp, ReplicatedState};
