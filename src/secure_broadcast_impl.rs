78b308b757c2d55628ed8c673d5350fde66a2248
danda <dan-da@users.noreply.github.com>
2020-11-19 15:47:46 -0800

    refactor SecureBroadcastImpl and SecureBroadcastNetwork traits

--- src/secure_broadcast_impl.rs
+++ src/secure_broadcast_impl.rs
@@ -5,7 +5,7 @@ use ed25519::Keypair;
 
 use crate::{Actor, Packet, ReplicatedState, SecureBroadcastAlgorithm};
 
-pub trait SecureBroadcastImpl<A: SecureBroadcastAlgorithm>: Debug {
+pub trait SecureBroadcastImpl: Debug {
     type Algo: SecureBroadcastAlgorithm;
 
     fn new(known_peers: HashSet<Actor>) -> Self; 
@@ -14,17 +14,17 @@ pub trait SecureBroadcastImpl<A: SecureBroadcastAlgorithm>: Debug {
 
     fn actor(&self) -> Actor;
 
-    fn state(&self) -> ReplicatedState<A>;
+    fn state(&self) -> ReplicatedState<Self::Algo>;
 
     fn peers(&self) -> HashSet<Actor>;
 
-    fn request_membership(&self) -> Vec<Packet<A::Op>>;
+    fn request_membership(&self) -> Vec<Packet<<Self::Algo as SecureBroadcastAlgorithm>::Op>>;
 
-    fn sync_from(&mut self, state: ReplicatedState<A>);
+    fn sync_from(&mut self, state: ReplicatedState<Self::Algo>);
 
-    fn exec_algo_op(&self, f: impl FnOnce(&A) -> Option<A::Op>) -> Vec<Packet<A::Op>>;
+    fn exec_algo_op(&self, f: impl FnOnce(&Self::Algo) -> Option<<Self::Algo as SecureBroadcastAlgorithm>::Op>) -> Vec<Packet<<Self::Algo as SecureBroadcastAlgorithm>::Op>>;
 
-    fn read_state<V>(&self, f: impl FnOnce(&A) -> V) -> V;
+    fn read_state<V>(&self, f: impl FnOnce(&Self::Algo) -> V) -> V;
 
-    fn handle_packet(&mut self, packet: Packet<A::Op>) -> Vec<Packet<A::Op>>;
+    fn handle_packet(&mut self, packet: Packet<<Self::Algo as SecureBroadcastAlgorithm>::Op>) -> Vec<Packet<<Self::Algo as SecureBroadcastAlgorithm>::Op>>;
 }
