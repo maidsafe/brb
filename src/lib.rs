75a109eee8758fc6ccc8847da290b37a018286ae
danda <dan-da@users.noreply.github.com>
2020-12-22 12:16:53 -0800

    expose Actor, DeterministicBRB, Net, Packet, and BRBAlgorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 // #![deny(missing_docs)]
 
-
 pub mod actor;
+pub use actor::Actor;
+
 pub mod bft_membership;
+
 pub mod deterministic_brb;
+pub use deterministic_brb::DeterministicBRB;
+
 pub mod net;
+pub use net::Net;
+
 pub mod packet;
+pub use packet::Packet;
+
 pub mod brb_algorithm;
+pub use brb_algorithm::BRBAlgorithm;
