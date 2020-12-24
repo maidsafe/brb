d77561c67f0294cf7b006065781aeb3fc69ede51
danda <dan-da@users.noreply.github.com>
2020-12-23 17:12:18 -0800

    move actor.rs and bft_membership.rs into brb_membership crate, and add a dependency on it

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 // #![deny(missing_docs)]
 
-pub mod actor;
-pub use actor::Actor;
-
-pub mod bft_membership;
+// re-export these
+pub use brb_membership::{Actor, Sig, SigningActor};
 
 pub mod deterministic_brb;
 pub use deterministic_brb::DeterministicBRB;
@@ -12,7 +10,7 @@ pub mod net;
 pub use net::Net;
 
 pub mod packet;
-pub use packet::Packet;
+pub use packet::{Packet, Payload};
 
 pub mod brb_algorithm;
 pub use brb_algorithm::BRBAlgorithm;
