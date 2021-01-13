1f1d5770fe0cc9befe50589617dac19a2f4be7c1
David Rusu <davidrusu@protonmail.com>
2021-01-13 11:00:44 -0500

    Revert "Revert "Allow BRBDT's to return structured validation responses""

--- src/brb_data_type.rs
+++ src/brb_data_type.rs
@@ -7,12 +7,13 @@ use crate::Actor;
 
 pub trait BRBDataType: Debug {
     type Op: Debug + Clone + Hash + Eq + Serialize;
+    type Validation: Debug + 'static;
 
     /// initialize a new replica of this datatype
     fn new(actor: Actor) -> Self;
 
     /// Protection against Byzantines
-    fn validate(&self, from: &Actor, op: &Self::Op) -> bool;
+    fn validate(&self, source: &Actor, op: &Self::Op) -> Result<(), Self::Validation>;
 
     /// Executed once an op has been validated
     fn apply(&mut self, op: Self::Op);
