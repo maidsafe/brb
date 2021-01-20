c9e3e924666df3cf1c25b3aab44e12727868127d
danda <dan-da@users.noreply.github.com>
2021-01-20 15:13:59 -0800

    doc/all: documenting and cleanup.
    * Adds maidsafe copyright notice to *.rs
    * Adds #![deny(missing_docs)] to lib.rs
    * Adds comment docs for all structs/enums/code
    * Adds std::error::Error trait as requirement for BRBDataType::validate()
    * renames DeterministicBRB::quorum() to supermajority()

--- src/brb_data_type.rs
+++ src/brb_data_type.rs
@@ -1,18 +1,43 @@
+// Copyright 2021 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
+
+//! The purpose of BRB is to provide a BFT transport for CRDT-esque Data Types.
+//!
+//! The BRBDataType trait defines the contract such Data Types must fulfill.
+//! Typically, an existing CRDT algorithm wiil be wrapped by a struct that
+//! implements this trait.
+//!
+//! Examples of types that implement BRBDataType:
+//!   brb_dt_orswot, brb_dt_at2, brb_dt_tree
+
+use std::error::Error;
 use std::fmt::Debug;
 use std::hash::Hash;
 
 use serde::Serialize;
 
+/// The BRBDataType trait
 pub trait BRBDataType<A>: Debug {
+    /// The set of ops this data type accepts
     type Op: Debug + Clone + Hash + Eq + Serialize;
-    type ValidationError: Debug + 'static;
+
+    /// A validation error specific to this data type.
+    type ValidationError: Debug + Error + 'static;
 
     /// initialize a new replica of this datatype
     fn new(actor: A) -> Self;
 
     /// Protection against Byzantines
+    /// Validate any incoming operations, here you must perform your byzantine fault
+    /// tolerance checks specific to your algorithm    
     fn validate(&self, source: &A, op: &Self::Op) -> Result<(), Self::ValidationError>;
 
-    /// Executed once an op has been validated
+    /// Execute an op after it has been validated.
     fn apply(&mut self, op: Self::Op);
 }
--- src/packet.rs
+++ src/packet.rs
@@ -1,23 +1,47 @@
+// Copyright 2021 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
+
+//! BRB uses Packet and Payload types that are not specific to any network transport
+//! layer such as tcp/ip. As such, BRB may easily be adapted to work over various
+//! transports.
+
 use serde::{Deserialize, Serialize};
 
 use crate::deterministic_brb;
 use crate::{Actor, Sig};
 
+/// Represents a logical message packet with a BRB specific payload.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Packet<A: Actor<S>, S: Sig, Op> {
+    /// source actor
     pub source: A,
+    /// destination actor
     pub dest: A,
+    /// payload data
     pub payload: Payload<A, S, Op>,
+    /// signature of payload data by source actor
     pub sig: S,
 }
 
+/// Enumerates types of BRB data that may be included in a Packet.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Payload<A: Actor<S>, S: Sig, DataTypeOp> {
+    /// Represents an AntiEntropy request
     AntiEntropy {
+        /// last-seen generation
         generation: brb_membership::Generation,
+        /// delivered clock
         delivered: crdts::VClock<A>,
     },
+    /// Represents a BRB operation
     BRB(deterministic_brb::Op<A, S, DataTypeOp>),
     // Box to avoid https://rust-lang.github.io/rust-clippy/master/index.html#large_enum_variant
+    /// Represents a brb_membership Vote
     Membership(Box<brb_membership::Vote<A, S>>),
 }
